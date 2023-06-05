650ef2f491f1a29299ca2d5ad4f580079e2380c2
parichehrmohebbi <parichehr.mohebbi@yahoo.com>
2023-06-06 00:27:55 +0200

    install packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  //cause webpack does not know about how to bundle the graphql , we use another loader for it
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.graphql?$/,
+      loader: "webpack-graphql-loader",
+    });
+
+    return config;
+  },
+};
