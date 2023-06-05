c0a938c5480401904cf1b4f8e582b7d087ce7551
parichehrmohebbi <parichehr.mohebbi@yahoo.com>
2023-06-06 00:30:41 +0200

    create my own types and resolvers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,72 +1,18 @@
-import { Resolvers, Word } from "./types";
-import { connect } from "../dao";
-import { WordDbObject } from "../dao/types";
+import { connect } from "@src/dao";
 
 const dbPromise = connect();
 
 const getCollection = async () => {
   const db = await dbPromise;
-  console.log("worrrrrds", db.collection<WordDbObject>("words"));
-  return db.collection<WordDbObject>("words");
+  return db.collection("words");
 };
 
-const fromDbObject = (dbObject: WordDbObject): Word => ({
-  wordId: dbObject._id.toHexString(),
-  title: dbObject.title,
-  description: dbObject.description,
-  author: dbObject.author,
-  relatedLinks: dbObject.relatedLinks,
-  relatedTopics: dbObject.relatedTopics,
-});
-
-const resolvers: Resolvers = {
+const resolvers = {
   Query: {
-    allWords: async () => {
+    async words() {
       const collection = await getCollection();
-      return await collection.find().map(fromDbObject).toArray();
+      return collection.find().toArray();
     },
-    word: async (_: any, { wordId }) => {
-      const collection = await getCollection();
-      const dbObject = await collection.findOne(
-        (x: WordDbObject) => x._id.toString() === wordId
-      );
-      return dbObject ? fromDbObject(dbObject) : null;
-    },
-  },
-  Mutation: {
-    createWord: async (
-      _: any,
-      { title, description, author, relatedLinks, relatedTopics }
-    ) => {
-      const data: Omit<WordDbObject, "_id"> = {
-        title: title,
-        description,
-        author,
-        relatedLinks,
-        relatedTopics,
-      };
-
-      const collection = await getCollection();
-      const document = await collection.insertOne(data as WordDbObject);
-      return fromDbObject({
-        ...data,
-        _id: document.insertedId,
-      });
-    },
-    // updateWord: async (_: any, { wordId, data }) => {
-    //   const collection = await getCollection();
-    //   const result = await collection.findOneAndUpdate(
-    //     {
-    //       _id: wordId,
-    //     },
-    //     { $set: data },
-    //     {
-    //       returnOriginal: false,
-    //     }
-    //   );
-
-    //   return fromDbObject(result.value);
-    // },
   },
 };
 
