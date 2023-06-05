fa6c1366982aefb5c7fbeb8ad82fcdfa9de10d33
parichehrmohebbi <parichehr.mohebbi@yahoo.com>
2023-06-05 12:00:24 +0200

    appolo client config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/      styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import fetch from "node-fetch"; // Use node-fetch here to allow SSR
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "/api/graphql", fetch: fetch as any }),
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
