038241d54a4e2a8532c49b5666f21221f54ed684
parichehrmohebbi <parichehr.mohebbi@yahoo.com>
2023-06-05 12:01:02 +0200

    appolo server config with introspection

---
+++ pages/api/graphql.ts
@@ -0,0 +1,20 @@
+import { ApolloServer } from "@apollo/server";
+import "graphql-import-node";
+import typeDefs from "@src/graphql/schema.graphql";
+import resolvers from "@src/graphql/resolvers";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { DIRECTIVES } from "@graphql-codegen/typescript-mongodb";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const apolloServer = new ApolloServer({
+  introspection: true,
+  typeDefs: [DIRECTIVES, typeDefs],
+  resolvers,
+});
+
+export default startServerAndCreateNextHandler(apolloServer);
