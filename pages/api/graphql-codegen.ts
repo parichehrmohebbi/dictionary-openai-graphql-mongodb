c37442cda99092f27d36d2ba54ccc9566778ef13
parichehrmohebbi <parichehr.mohebbi@yahoo.com>
2023-06-06 00:29:06 +0200

    remove codegen files and resolver file which was generated by codegen

---
+++ pages/api/graphql-codegen.ts
@@ -0,0 +1,30 @@
+// import { ApolloServer } from "@apollo/server";
+// import "graphql-import-node";
+// import typeDefs from "@src/graphql/schema.graphql";
+// import resolvers from "@src/graphql/resolvers";
+// import { startServerAndCreateNextHandler } from "@as-integrations/next";
+// import { DIRECTIVES } from "@graphql-codegen/typescript-mongodb";
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+// const apolloServer = new ApolloServer({
+//   typeDefs: [DIRECTIVES, typeDefs],
+//   resolvers,
+// });
+
+// export default startServerAndCreateNextHandler(apolloServer);
+
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import typeDefs from "@src/graphql/types";
+import resolvers from "@src/graphql/resolvers";
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+export default startServerAndCreateNextHandler(apolloServer);
