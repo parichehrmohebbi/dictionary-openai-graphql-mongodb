6e602cf9c6515aa87f651d8d0a6f96049735cb53
parichehrmohebbi <parichehr.mohebbi@yahoo.com>
2023-06-21 13:24:22 +0200

    add chatgpt

---
+++ src/chatGPT/getResponse.ts
@@ -0,0 +1,37 @@
+import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
+import { PromptType } from "./propmtType";
+import { Maybe } from "@src/dao/types";
+
+const CHATGPT_API_KEY = "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";
+
+export const chatGPTResponse = async (
+  promptType: PromptType,
+  technology: string
+): Promise<Maybe<string>> => {
+  const prompt = promptType.replace("{0}", technology);
+
+  const messages: ChatCompletionRequestMessage[] = [];
+  messages.push({ role: "user", content: prompt });
+
+  const configuration = new Configuration({
+    apiKey: CHATGPT_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      n: 1,
+      stream: false,
+    });
+    console.log("chatbox answer", completion);
+    const response = completion.data.choices[0].message;
+
+    return response?.content;
+  } catch (error) {
+    console.error(`Failed to generate response for:${technology}`, error);
+    return "";
+  }
+};
---
+++ src/chatGPT/propmtType.ts
@@ -0,0 +1,6 @@
+export enum PromptType {
+  IS_RELATED = "give my answer with yes or no, is {0} related to web development or computer science or software technologies?",
+  ASK_DEFINITION = "What is {0}?",
+  ASK_RELATED_LINKS = "give me the related links array to {0} with titles ",
+  ASK_RELATED_TOPICS = "give me the first 10 related topics array to {0}",
+}
