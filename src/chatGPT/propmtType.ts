0ac70e1dea52409d34456f28e1e2146f24c29785
Parichehr <parichehr.mohebbi.ext@luminus.be>
2023-11-07 13:48:40 +0100

    givee

--- src/chatGPT/propmtType.ts
+++ src/chatGPT/propmtType.ts
@@ -1,5 +1,5 @@
 export enum PromptType {
-  IS_RELATED_PROMPT = "give my answer with yes or no. do not write any explanation or comment. Just write yes or no.  is {0} related to web development or computer science or software technologies?",
+  IS_RELATED_PROMPT = "givee my answer with yes or no. do not write any explanation or comment. Just write yes or no.  is {0} related to web development or computer science or software technologies?",
   DEFINITION_PROMPT = "What is {0}? give me an extensive definition. do not write any explanations or comment. just the definition.I need verbosity to be high ",
   RELATED_LINKS_PROMPT = "give me the list of top 10 related links to {0} in JSON . do not write any explanations or comment. just the list including title and link. ",
   RELATED_TOPICS_PROMPT = "give me the list of top 10 related topics to {0} in JSON . do not write any explanations or comment. just the list including title .",
